0789e7ae55fd395d31e2cc21692215c8a33dcefd
Merveux <mervinkwizera@gmail.com>
2025-11-17 14:21:49 +0200

    Create Question9.py

---
+++ Question9.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+d=int(input("Enter the fourth number:"))
+e=int(input("Enter the fifth number:"))
+list=[a,b,c,d,e]
+print(list[0])
+print(list[4])
+print(list)
