eeb40432c84fb3eed365282fe290eefbfe8fc5c9
Merveux <mervinkwizera@gmail.com>
2025-11-17 15:42:52 +0200

    Create Question22.py

---
+++ Question22.py
@@ -0,0 +1,5 @@
+for i in range(1,31)
+ list.append(i)
+multiples_of_5=[num for num in list if num%5==0]
+multiples_of_3=[num for num in list if num%3==0]
+multiples_of_5and3=[num for num in list if num%5==0 && num%3==0]
